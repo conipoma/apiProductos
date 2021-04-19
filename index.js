7c5776023185b7ce8c29daaa9642ad8e2be34a04
Constanza <coni.poma@gmail.com>
2021-04-18 21:18:48 -0400

    fix user on mongo

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const models = require('./models/productModel')(app, mongoose);
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
-// Connection to DB
-mongoose.connect('mongodb://productListUser:productListPassword@localhost:27017/promotions', function(err, res) {
-  if(err) throw err;
-  console.log('Connected to Database');
+
+
+mongoose.connect("mongodb://localhost:27017/promotions", {
+    "auth": { "authSource": "admin" },
+    "user": "productListUser",
+    "pass": "productListPassword"
 });
 
 // middlewares
